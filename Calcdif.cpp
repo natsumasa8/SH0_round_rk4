5ef7dee309ded47a90b3e43a6457e531135fe51a
natsumasa <natsumasa@email.com>
2023-09-19 15:57:06 +0900

    input change

--- Calcdif.cpp
+++ Calcdif.cpp
@@ -22,7 +22,7 @@ void Calcdif::readinput(){
 	fin >> dummy; fin >> N_output; std::cout << "N_output: " << N_output << std::endl;
 	
 	fin >> dummy; fin >> dx; std::cout << "dx: " << dx << std::endl;
-	fin >> dummy; fin >> dx; std::cout << "dx: " << dx << std::endl;
+	fin >> dummy; fin >> Nx; std::cout << "Nx: " << Nx << std::endl;
 
 	fin >> dummy; fin >> initial_amp; std::cout << "initial amplitude: " << initial_amp << std::endl;
 	
