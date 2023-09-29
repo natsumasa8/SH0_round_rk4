0f6d5f5c77293aebb7f03a5aa46b776a5fd0be9b
natsumasa <natsumasa@email.com>
2023-09-29 14:11:36 +0900

    CalcSH -> rk4

--- SHsimulate.cpp
+++ SHsimulate.cpp
@@ -32,53 +32,36 @@ void SHsimulate::ConstractParameters(){
 }
 
 //note: clc -> s.row() etc...
-Eigen::MatrixXd SHsimulate::CalcSH(Eigen::MatrixXd& s, Calcdif& clc){
+Eigen::MatrixXd SHsimulate::SH_rt(Eigen::MatrixXd& s, Calcdif& clc){
     Eigen::MatrixXd tmp = Eigen::MatrixXd::Zero(clc.Nx,clc.Nx);
 #pragma omp parallel for
     for (int p=0;p<clc.Nx;p++){
+#pragma omp parallel for
         for (int q=0;q<clc.Nx;q++){
             tmp(p,q) += -clc.laplacian(p,q,s);
             tmp(p,q) += -clc.laplacian2(p,q,s);
             tmp(p,q) += -a * s(p,q);
             tmp(p,q) += -b * std::pow(s(p,q),2.0);
             tmp(p,q) += -c * std::pow(s(p,q),3.0);
-            
-            tmp(p,q) *= clc.dt;
-            tmp(p,q) += s(p,q);
-            // tmp(p,q) += rand; //gaussian white noise ?? every time? 
         }
     }
     return tmp;
 }
 
-//runge kutta 4 version
-// Eigen::MatrixXd SHsimulate::CalcSH_rk4(Eigen::MatrixXd& s){
-// #pragma omp parallel for
-//     for (int i=0;i<clc.Nx;i++){
-//         for (int j=0;j<clc.Nx;j++){
+Eigen::MatrixXd SHsimulate::CalcSH_rk4(Eigen::MatrixXd& s, Calcdif& clc){
+    Eigen::MatrixXd k1, k2, k3, k4;
+    Eigen::MatrixXd s_half1, s_half2, s_one;
+    double h = clc.dt;
 
-//         }
-//     }
-// }
+    k1 = SH_rt(s, clc); //傾き算出
+    s_half1 = s + k1 * h * 0.5; //交点
+    k2 = SH_rt(s_half1, clc);
+    s_half2 = s + k2 * h * 0.5;
+    k3 = SH_rt(s_half2, clc);
+    s_one = s + k3 * h;
+    k4 = SH_rt(s_one, clc);
 
-//初期緩和計算用
-//note: clc -> s.row() etc...
-Eigen::MatrixXd SHsimulate::CalcRX(Eigen::MatrixXd& s, Calcdif& clc){
-    Eigen::MatrixXd rx = Eigen::MatrixXd::Zero(clc.Nx,clc.Nx);
-#pragma omp parallel for
-    for (int p=0;p<clc.Nx;p++){
-        for (int q=0;q<clc.Nx;q++){
-            rx(p,q) += -clc.laplacian(p,q,s);
-            rx(p,q) += -clc.laplacian2(p,q,s);
-            rx(p,q) += -a * s(p,q);
-            rx(p,q) += -b * std::pow(s(p,q),2.0);
-            rx(p,q) += -c * std::pow(s(p,q),3.0);
-            
-            rx(p,q) *= clc.dt;
-            rx(p,q) += s(p,q);
-        }
-    }
-    return rx;
+    return s+h*(k1+2.0*k2+2.0*k3+k4)/6.0;
 }
 
 //round boundary condition
@@ -87,6 +70,7 @@ void SHsimulate::round_boundary(Eigen::MatrixXd& s, double b){
     double r = (double)s.rows() / 2.0 - 2.0;
 #pragma omp parallel for	
     for(int i=0;i<s.rows();i++){
+#pragma omp parallel for	
         for(int j=0;j<s.rows();j++){
             // メッシュ座標から円の中心までの距離を計算
             double distance = std::pow(i - s.rows()/2, 2) + std::pow(j - s.rows()/2, 2);
@@ -102,6 +86,7 @@ Eigen::MatrixXd SHsimulate::CalcED(Eigen::MatrixXd& s, Eigen::MatrixXd& gs_x, Ei
     Eigen::MatrixXd tmp = Eigen::MatrixXd::Zero(clc.Nx,clc.Nx);
 #pragma omp paralle for
 	for (int i=0;i<clc.Nx;i++){
+#pragma omp parallel for	
         for (int j=0;j<clc.Nx;j++){
             tmp(i,j) += c * std::pow(s(i,j),4.0) / 4.0;
             tmp(i,j) += b * std::pow(s(i,j),3.0) / 3.0;
